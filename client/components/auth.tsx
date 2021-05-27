cb4fa07f65acc10d94617c8a272a7467abe17eaf
Sihamais <sihamais98@gmail.com>
2021-05-27 10:49:12 +0200

    Correct layout

--- client/components/auth.tsx
+++ client/components/auth.tsx
@@ -39,7 +39,7 @@ export default function Auth() {
                 <button className="bg-gray-600 text-gray-100 w-1/2 p-2 
                     rounded-lg m-auto hover:bg-gray-500 focus:ring-2 focus:ring-gray-600 focus:ring-opacity-50 font-semibold">Sign up</button>
                 <a className="text-center underline font-semibold cursor-pointer text-blue-600 hover:text-blue-500" onClick={SwitchPage}>
-                    Already have an account ? Sign In.</a>
+                    Already have an account ? <br></br>Sign In.</a>
             </div>
         </div>
     )
@@ -47,7 +47,7 @@ export default function Auth() {
 
     function SignIn(){
         return (
-            <div className="shadow-lg rounded-md bg-gray-100 w-auto m-auto justify-center space-y-8 flex flex-col p-10">
+            <div className="shadow-lg rounded-md bg-gray-100 m-auto justify-center space-y-8 flex flex-col p-10">
                 <div className="w-full space-y-3 flex flex-col m-auto font-semibold">
                     <div className="flex flex-col">
                         <label className="text-gray-600">Username or Email</label>
@@ -62,18 +62,18 @@ export default function Auth() {
                     <button className="bg-gray-600 text-gray-100 w-1/2 p-2 
                     rounded-lg m-auto hover:bg-gray-500 focus:outline-none focus:ring-2 focus:ring-gray-600 focus:ring-opacity-50 font-semibold">Sign in</button>
                     <a className="text-center underline font-semibold cursor-pointer text-blue-600 hover:text-blue-500" onClick={SwitchPage}>
-                        Don't have an account ? Sign Up</a>
+                        Don't have an account ? <br></br>Sign Up</a>
                 </div>
             </div>
         )
     }
     
     return(
-        <div className="cont w-screen h-screen bg-indigo-700 content-center justify-center">
-            <div className="flex md:flex-row flex-col m-auto h-screen md:w-2/3 md:space-x-10 md:space-x-0">
-                <div className="font-extrabold text-5xl tracking-tight m-auto grid justify-center w-1/2">
+        <div className="w-screen min-h-screen h-auto bg-indigo-700 content-center justify-center">
+            <div className="flex md:flex-row flex-col m-auto min-h-screen h-auto content-center md:w-2/3 md:space-x-10 p-5">
+                <div className="font-extrabold text-5xl tracking-tight mx-auto grid justify-center content-center w-1/2 mb-5">
                     <h1 className="text-gray-100">Strugl</h1>
-                    <h1 className="text-black text-4xl">Your new decentralized social network</h1>
+                    <h1 className="text-black text-4xl">Your privacy-conscious social network</h1>
                 </div>
                 {currentPage}
             </div>
