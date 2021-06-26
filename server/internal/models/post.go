e172efdf0287c681c25efdd3effdb3f2bc26a2d7
Paco <kpaco@pm.me>
2021-06-26 02:42:36 +0200

    Post date_updated json null serialization

--- server/internal/models/post.go
+++ server/internal/models/post.go
@@ -1,6 +1,9 @@
 package models
 
-import "database/sql"
+import (
+	"database/sql"
+	"encoding/json"
+)
 
 type Post struct {
 	ID          int64          `json:"id" db:"post_id"`
@@ -8,5 +11,32 @@ type Post struct {
 	Author_ID   int64          `json:"author_id" db:"user_id"`
 	Content     string         `json:"content" db:"content"`
 	DateCreated string         `json:"date_created" db:"date_created"`
-	DateUpdated sql.NullString `json:"date_updated" db:"date_updated"`
+	DateUpdated JsonNullString `json:"date_updated" db:"date_updated"`
+}
+
+type JsonNullString struct {
+    sql.NullString
+}
+
+func (v JsonNullString) MarshalJSON() ([]byte, error) {
+    if v.Valid {
+        return json.Marshal(v.String)
+    } else {
+        return json.Marshal(nil)
+    }
+}
+
+func (v *JsonNullString) UnmarshalJSON(data []byte) error {
+    // Unmarshalling into a pointer will let us detect null
+    var x *string
+    if err := json.Unmarshal(data, &x); err != nil {
+        return err
+    }
+    if x != nil {
+        v.Valid = true
+        v.String = *x
+    } else {
+        v.Valid = false
+    }
+    return nil
 }
