589d1559ae5fbe150e4b51f36fe1632bee205659
Paco <kpaco@pm.me>
2021-09-02 16:25:00 +0200

    added author pname to posts

--- server/internal/models/post.go
+++ server/internal/models/post.go
@@ -8,6 +8,7 @@ import (
 type Post struct {
 	ID          int64          `json:"id" db:"post_id"`
 	Author      string         `json:"author" db:"username"`
+	AuthorPName string         `json:"author_pname" db:"profile_name"`
 	Avatar      string         `json:"avatar" db:"avatar"`
 	Author_ID   int64          `json:"author_id" db:"user_id"`
 	Content     string         `json:"content" db:"content"`
