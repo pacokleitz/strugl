d51fdca72e06a1e94cb501a601d6b2feef43edeb
sihamais <sihamais98@gmail.com>
2021-09-08 10:42:18 +0200

    Test without security

--- client/next.config.js
+++ client/next.config.js
@@ -1,32 +1,32 @@
 const securityHeaders = [
-  {
-    key: "X-DNS-Prefetch-Control",
-    value: "on",
-  },
-  {
-    key: "Strict-Transport-Security",
-    value: "max-age=63072000; includeSubDomains; preload",
-  },
-  {
-    key: "X-XSS-Protection",
-    value: "1; mode=block",
-  },
-  {
-    key: "X-Frame-Options",
-    value: "SAMEORIGIN",
-  },
-  {
-    key: "Permissions-Policy",
-    value: "camera=(), microphone=(), geolocation=()",
-  },
-  {
-    key: "X-Content-Type-Options",
-    value: "nosniff",
-  },
-  {
-    key: "Referrer-Policy",
-    value: "origin-when-cross-origin",
-  },
+  // {
+  //   key: "X-DNS-Prefetch-Control",
+  //   value: "on",
+  // },
+  // {
+  //   key: "Strict-Transport-Security",
+  //   value: "max-age=63072000; includeSubDomains; preload",
+  // },
+  // {
+  //   key: "X-XSS-Protection",
+  //   value: "1; mode=block",
+  // },
+  // {
+  //   key: "X-Frame-Options",
+  //   value: "SAMEORIGIN",
+  // },
+  // {
+  //   key: "Permissions-Policy",
+  //   value: "camera=(), microphone=(), geolocation=()",
+  // },
+  // {
+  //   key: "X-Content-Type-Options",
+  //   value: "nosniff",
+  // },
+  // {
+  //   key: "Referrer-Policy",
+  //   value: "origin-when-cross-origin",
+  // },
 ];
 
 module.exports = {
